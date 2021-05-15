2bb183fb21c994f6523eda1a49dbede764549a23
Irimia Cosmin-Iulian <irimia.cosmin@gmail.com>
2021-05-15 03:38:43 +0300

    Added async versions of all the methods in the DSUService and fixed some more things.

--- utilitaries/code/scripts/services/DSUService.js
+++ utilitaries/code/scripts/services/DSUService.js
@@ -2,16 +2,13 @@ export default class DSUService {
 
     PATH = "/";
 
-    constructor(DSUStorage, path) {
+    constructor(DSUStorage, path = this.PATH) {
         this.DSUStorage = DSUStorage;
         this.PATH = path;
     }
 
     getEntities(path, callback) {
-        if (typeof path === 'function') {
-            callback = path;
-            path = this.PATH;
-        }
+        [path, callback] = this.swapParamsIfPathIsMissing(path, callback);
         this.DSUStorage.call('listDSUs', path, (err, dsuList) => {
             if (err) {
                 return callback(err, undefined);
@@ -41,11 +38,12 @@ export default class DSUService {
         })
     }
 
+    async getEntitiesAsync(path) {
+        return this.asyncMyFunction(this.getEntities, [...arguments])
+    }
+
     getEntity(uid, path, callback) {
-        if (typeof path === 'function') {
-            callback = path;
-            path = this.PATH;
-        }
+        [path, callback] = this.swapParamsIfPathIsMissing(path, callback);
         this.DSUStorage.getItem(this._getDsuStoragePath(uid, path), (err, content) => {
             if (err) {
                 return callback(err, undefined);
@@ -55,11 +53,12 @@ export default class DSUService {
         })
     }
 
+    async getEntityAsync(uid, path) {
+        return this.asyncMyFunction(this.getEntity, [...arguments])
+    }
+
     saveEntity(entity, path, callback) {
-        if (typeof path === 'function') {
-            callback = path;
-            path = this.PATH;
-        }
+        [path, callback] = this.swapParamsIfPathIsMissing(path, callback);
         this.DSUStorage.call('createSSIAndMount', path, (err, keySSI) => {
             if (err) {
                 return callback(err, undefined);
@@ -70,11 +69,12 @@ export default class DSUService {
         })
     }
 
+    async saveEntityAsync(entity, path) {
+        return this.asyncMyFunction(this.saveEntity, [...arguments])
+    }
+
     updateEntity(entity, path, callback) {
-        if (typeof path === 'function') {
-            callback = path;
-            path = this.PATH;
-        }
+        [path, callback] = this.swapParamsIfPathIsMissing(path, callback);
         this.DSUStorage.setObject(this._getDsuStoragePath(entity.uid, path), entity, (err) => {
             if (err) {
                 return callback(err, undefined);
@@ -83,7 +83,12 @@ export default class DSUService {
         })
     }
 
-    mountEntity(keySSI, callback, path = this.PATH) {
+    async updateEntityAsync(entity, path) {
+        return this.asyncMyFunction(this.updateEntity, [...arguments])
+    }
+
+    mountEntity(keySSI, path, callback) {
+        [path, callback] = this.swapParamsIfPathIsMissing(path, callback);
         this.DSUStorage.call('mount', path, keySSI, (err) => {
             this.getEntity(keySSI, (err, entity) => {
                 if (err) {
@@ -94,7 +99,12 @@ export default class DSUService {
         })
     }
 
-    unmountEntity(uid, callback, path = this.PATH) {
+    async mountEntityAsync(keySSI, path) {
+        return this.asyncMyFunction(this.mountEntity, [...arguments])
+    }
+
+    unmountEntity(uid, path, callback) {
+        [path, callback] = this.swapParamsIfPathIsMissing(path, callback);
         let unmountPath = path + '/' + uid;
         this.DSUStorage.call('unmount', unmountPath, (err, result) => {
             if (err) {
@@ -104,7 +114,26 @@ export default class DSUService {
         });
     }
 
+    async unmountEntityAsync(uid, path) {
+        return this.asyncMyFunction(this.unmountEntity, [...arguments])
+    }
+
     _getDsuStoragePath(keySSI, path = this.PATH) {
         return path + '/' + keySSI + '/data.json';
+    }
+
+    swapParamsIfPathIsMissing(path, callback) {
+        return typeof path === 'function' ? [this.PATH, path] : [path, callback];
+    }
+
+    asyncMyFunction = (func, params) => {
+        return new Promise((resolve, reject) => {
+            func(...params, (err, data) => {
+                if (err) {
+                    return reject(err);
+                }
+                resolve(data);
+            })
+        })
     }
 }
