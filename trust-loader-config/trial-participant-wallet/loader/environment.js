ed5631d43110a110f0ccdfe632e8906c7af090a0
mastaleru <mastaleru.rafael@gmail.com>
2021-11-19 12:46:25 +0200

    linked trial-participant-ssapp into workspace

---
+++ trust-loader-config/trial-participant-wallet/loader/environment.js
@@ -0,0 +1,15 @@
+export default {
+  appName: 'patient-wallet',
+  vault: 'server',
+  agent: 'browser',
+  system: 'any',
+  browser: 'any',
+  mode: 'autologin',
+  domain: 'vault',
+  sw: true,
+  pwa: true,
+  didPrompt: true,
+  workspace: 'eco',
+  'legenda for properties':
+    ' vault:(server, browser) agent:(mobile, browser) system:(iOS, Android, any) browser:(Chrome, Firefox, any) mode:(development, release) sw:(true, false) pwa:(true, false)',
+};
