167e0397141464a49a57985caef99abaf91c2895
mastaleru <mastaleru.rafael@gmail.com>
2022-01-28 15:47:06 +0200

    cleanup

--- trust-loader-config/clinical-site-wallet/loader/environment.js
+++ trust-loader-config/clinical-site-wallet/loader/environment.js
@@ -10,7 +10,6 @@ export default {
   enclaveType:"WalletDBEnclave",
   sw: false,
   pwa: false,
-  didPrompt: true,
   workspace: 'eco',
   'legenda for properties':
     ' vault:(server, browser) agent:(mobile, browser) system:(iOS, Android, any) browser:(Chrome, Firefox, any) mode:(development, release) sw:(true, false) pwa:(true, false)',
