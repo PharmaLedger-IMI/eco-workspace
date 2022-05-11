7ef3e557d5369e379e0c5e8cdff9482932ab81cf
paiusCatalin <paiusCatalin>
2022-05-11 09:53:18 +0300

    deployment preparation, added webcardinal and cleanup unused files

--- deployment/docker/start.sh
+++ deployment/docker/start.sh
@@ -2,8 +2,8 @@
 name="$(./util/name.sh -1)"
 
 docker run --detach \
-    --hostname epi \
+    --hostname eco \
     --publish 8080:8080 \
     --name $name \
     --restart always \
-    pharmaledger/epi
+    paiuscatalin/eco
