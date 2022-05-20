4c2361e04a174a5f136ba593d6130fb75696b30f
mastaleru <mastaleru.rafael@gmail.com>
2022-05-20 13:22:39 +0300

    moved sponsor ssapp and theme in separate repositories

--- sponsor-ssapp/code/scripts/constants/topics.js
+++
@@ -1,6 +0,0 @@
-export const Topics = {
-  RefreshParticipants: 'refresh-participants-for-',
-  RefreshTable: 'refresh-table',
-  RefreshTrialDetails: 'refresh-trial-details-for-',
-  RefreshTrialConsents: 'refresh-trial-consents',
-};
--- sponsor-ssapp/code/scripts/constants/trialDetails.js
+++
@@ -1,6 +0,0 @@
-export const menuOptions = {
-  TrialDetails: 'Trial Details',
-  CountriesSites: 'Countries & Sites',
-  Consents: 'Inform Consent Forms',
-  // VisitsProcedures: 'Visits and Procedures',
-};
